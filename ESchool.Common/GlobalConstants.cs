5a09312b07a9184882d243f5d5596301b99ab971
KrumDelinov <krum.delinov@gmail.com>
2020-03-08 15:11:19 +0200

    First project commit

---
+++ ESchool.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace ESchool.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "ESchool";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
